474233310d9830c07334fbea088ee2e71e47b409
JP <71121804+JPDevAcc@users.noreply.github.com>
2023-01-25 13:24:49 +0000

    Integrated 'quizListUIManager' class + new test data

--- quizzes/quizList.js
+++ quizzes/quizList.js
@@ -2,9 +2,15 @@
 
 export default [
 	{
-		name: 'test',
-		title: 'Test Quiz Title',
-		summary: 'Have a go at this quiz!',
-		imageSrc: 'some_image_url'
+		name: 'test1',
+		title: 'Test1 Quiz Title',
+		summary: 'Have a go at this quiz number one!',
+		imageSrc: 'https://images.unsplash.com/photo-1507413245164-6160d8298b31?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+	},
+	{
+		name: 'test2',
+		title: 'Test2 Quiz Title',
+		summary: 'Have a go at this quiz number two!',
+		imageSrc: 'https://images.unsplash.com/photo-1609619385076-36a873425636?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
 	},
 ] ;
